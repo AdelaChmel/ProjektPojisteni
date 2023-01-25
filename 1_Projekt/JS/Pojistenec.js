925b7cf5d2b3939cd3dd055336cf5a881883698e
AdelaChmel <122735422+AdelaChmel@users.noreply.github.com>
2023-01-25 14:42:18 +0100

    Add files via upload

---
+++ 1_Projekt/JS/Pojistenec.js
@@ -0,0 +1,14 @@
+'use strict'
+
+class Pojistenec {
+
+    constructor(jmeno, prijmeni, vek, telefon) {
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.celeJmeno = jmeno + " " + prijmeni;
+        this.vek = vek;
+        this.telefon = telefon;
+    }
+}
+
+
---
+++ 1_Projekt/JS/obsluha.js
@@ -0,0 +1,5 @@
+'use strict'
+import { PojisteniApp } from "./PojisteniApp.js";
+const pojisteniApp = new PojisteniApp();
+pojisteniApp.vytvorTabulku();
+//localStorage.removeItem("pojistenci");
